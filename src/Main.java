2a06f974d77110649318dea903b2afa05b4fd890
Ana Paula <oliveiraanapaula6012@gmail.com>
2023-06-14 09:07:20 -0300

    geraçao de planta com semente

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //Aula introdutória de Orientação à Objetos{
-        //declaracao do objeto
-        Pessoa adao;
-        //instanciaçao do objeto
-        adao = new Pessoa();
-        // ao mesmo tempo
-        Pessoa qualquer = new Pessoa();
-        // definir o formato do objeto
-         qualquer.nome = "Rita";
-         qualquer.sobrenome = "lee";
-         qualquer.falar();
-        System.out.println(qualquer.falar("oi"));
-        // Criar novos objetos
-        Pessoa rainha;
-        // instanciar objetos
-         rainha = new Pessoa();
-         // definir forma do objeto
-        rainha.nome = "tina";
-        rainha.sobrenome = "tunner";
-        //definir do comportamento
-        rainha.falar();
-        rainha.comer();
+
     }
 }
---
+++ src/Planta.java
@@ -0,0 +1,11 @@
+public class Planta {
+    // construtores
+    Planta(Semente s1){
+
+
+        //sobrecarga do metodo construtor
+        Planta(Semente semente){
+
+        }
+    }
+}
